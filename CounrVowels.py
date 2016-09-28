6c6639b075c999e3b7fa2a98404cb33ff67aa7b2
Nazaf Anwar <anwar.najaf16@gmail.com>
2016-09-28 13:01:57 +0530

    Create CounrVowels.py

---
+++ CounrVowels.py
@@ -0,0 +1,9 @@
+count = 0
+
+#s is a variable containing a string of character
+
+for i in s:
+    if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
+        count += 1
+
+print("Number of vowels: ", count)
