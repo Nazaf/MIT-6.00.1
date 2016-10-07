bec21f091418a79b21e8827ec8c8e70ba6462ff2
Nazaf Anwar <anwar.najaf16@gmail.com>
2016-10-07 13:34:47 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -3,3 +3,15 @@
 
 def isWordGuessed(secretWord, lettersGuessed):
      return all(letter in lettersGuessed for letter in secretWord)
+
+
+# 2. Printing out the Users gusses. if the guess doesn't match, it replaces the string with _
+
+def getGuessedWord(secretWord, lettersGuessed):
+    newWord = ""
+    for i in range(len(secretWord)):
+        if secretWord[i] in lettersGuessed:
+            newWord += secretWord[i]
+        else:
+            newWord += " _ "
+    return newWord
